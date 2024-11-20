bf416a80a4b7c0c3a7dc6d5e212cf232654bcfb7
Gereon <gereon.risse@gmail.com>
2024-11-20 14:44:08 +0100

    added React Navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import {
-  SafeAreaView,
   Text,
+  View,
 } from 'react-native';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen Test</Text>
+    </View>
+  );
+}
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: 'Home',
+  screens: {
+    Home: HomeScreen,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Text>Test</Text>
-    </SafeAreaView>
+    // <SafeAreaView>
+    //   <Text>Testes</Text>
+    // </SafeAreaView>
+    <Navigation />
   );
 }
 
