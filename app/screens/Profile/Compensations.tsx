491730a8bf7ddb941f46467bd9cb6fe4f1cf13ca
Gereon <gereon.risse@gmail.com>
2024-12-05 14:56:50 +0100

    added Compensations Screen
    .

--- app/screens/Profile/Compensations.tsx
+++ app/screens/Profile/Compensations.tsx
@@ -1,7 +1,9 @@
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import PrimaryButton from '../../components/PrimaryButton.tsx';
 import React from 'react';
+import FormTextInput from '../../components/FormTextInput.tsx';
+import ErrorText from '../../components/ErrorText.tsx';
 
 
 export default function Compensations() {
@@ -9,17 +11,59 @@ export default function Compensations() {
 
   return (
     <SafeAreaView>
-      <Text>Compensations</Text>
+      <View style={styles.container}>
+        {/* Vouchers */}
+        <View style={styles.box}>
+          <Text style={styles.boxHeader}>Vouchers</Text>
 
-      <PrimaryButton
-        onPress={() => navigation.navigate('Vouchers')}
-        title='Vouchers'
-      />
+          <Text style={styles.boxText}>
+            Receive vouchers for various stores and services as a token of appreciation.
+          </Text>
 
-      <PrimaryButton
-        onPress={() => navigation.navigate('Money')}
-        title='Financial Compensation'
-      />
+          <PrimaryButton
+            onPress={() => navigation.navigate('Vouchers')}
+            title='Redeem'
+          />
+        </View>
+
+        {/* Money */}
+        <View style={styles.box}>
+          <Text style={styles.boxHeader}>Financial compensation</Text>
+
+          <Text style={styles.boxText}>
+            Receive a final compensation for your valuable participation.
+          </Text>
+
+          <PrimaryButton
+            onPress={() => navigation.navigate('Money')}
+            title='Redeem'
+          />
+        </View>
+      </View>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    gap: 20,
+  },
+  box: {
+    backgroundColor: '#d7d7d7',
+    borderRadius: 8,
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    gap: 12,
+  },
+  boxHeader: {
+    fontSize: 18,
+    fontWeight: 500,
+    textAlign: 'center',
+  },
+  boxText: {
+    fontSize: 16,
+    lineHeight: 20,
+    textAlign: 'justify',
+  },
+});
