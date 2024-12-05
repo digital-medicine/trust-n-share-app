f6755123b9265053d2de6f8d4bea56bcdc07a95c
Gereon <gereon.risse@gmail.com>
2024-12-05 16:57:12 +0100

    renamed the app to HealthNav

--- ios/HealthNavTests/HealthNavTests.m
+++ ios/HealthNavTests/HealthNavTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TrustNavigatorTests : XCTestCase
+@interface HealthNavTests : XCTestCase
 
 @end
 
-@implementation TrustNavigatorTests
+@implementation HealthNavTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
