de6743d40cdd3c6c25978c5f49cf6793be071c6f
Gereon <gereon.risse@gmail.com>
2024-12-18 15:19:21 +0100

    implemented Vouchers screen

--- app/screens/Compensations/Vouchers.tsx
+++ app/screens/Compensations/Vouchers.tsx
@@ -1,18 +1,59 @@
-import {Text, View} from 'react-native';
+import {Dimensions, StyleSheet, Text, View, ScrollView} from 'react-native';
 import {useUserStore} from '../../stores/user.ts';
-
 
 export default function VouchersScreen() {
   const user = useUserStore(state => state.user);
-  console.log(user);
+  // const user = {
+  //   availableCompensations: [
+  //     {
+  //       _id: '1',
+  //       name: 'Voucher 1',
+  //     },
+  //     {
+  //       _id: '2',
+  //       name: 'Voucher 2',
+  //     },
+  //     {
+  //       _id: '3',
+  //       name: 'Voucher 3',
+  //     },
+  //   ],
+  // }
 
   return (
-    <View>
-      <Text>VouchersScreen</Text>
-
-      {user?.availableCompensations.map(compensation => (
-        <Text key={compensation._id}>{compensation.name}</Text>
-      ))}
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.grid}>
+        {user?.availableCompensations.map(compensation => (
+          <View style={styles.gridItem} key={compensation._id}>
+            <Text style={styles.gridItemHeader}>{compensation.name}</Text>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  grid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 20,
+  },
+  gridItem: {
+    width: Dimensions.get('window').width / 2 - 30,
+    height: Dimensions.get('window').width / 2 - 30,
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  gridItemHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
